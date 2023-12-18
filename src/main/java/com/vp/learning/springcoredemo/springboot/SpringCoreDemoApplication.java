d5af949256d86d7d59c53adfb7682daf81b05193
vuthaphai <vuthaphai@gmail.com>
2023-12-18 22:03:21 +0700

    wip: bean in xml

--- src/main/java/com/vp/learning/springcoredemo/springboot/SpringCoreDemoApplication.java
+++ src/main/java/com/vp/learning/springcoredemo/springboot/SpringCoreDemoApplication.java
@@ -5,9 +5,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
-@ComponentScan("com.vp.learning.springcoredemo")
+//@ComponentScan("com.vp.learning.springcoredemo")
+@ImportResource("classpath:/spring/spring-config.xml")
 public class SpringCoreDemoApplication {
 
 	public static void main(String[] args) {
