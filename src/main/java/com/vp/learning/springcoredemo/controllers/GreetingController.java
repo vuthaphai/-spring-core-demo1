b11c4224ab7b8b81b28c6849deb51fbed1db77c7
vuthaphai <vuthaphai@gmail.com>
2023-12-18 21:54:01 +0700

    init commit bean xml

--- src/main/java/com/vp/learning/springcoredemo/controllers/GreetingController.java
+++ src/main/java/com/vp/learning/springcoredemo/controllers/GreetingController.java
@@ -1,21 +1,39 @@
 package com.vp.learning.springcoredemo.controllers;
 
 import com.vp.learning.springcoredemo.services.HelloWorldService;
+import com.vp.learning.springcoredemo.services.HelloWorldServiceGermanImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class GreetingController {
     private HelloWorldService helloWorldService;
+    private HelloWorldService helloWorldServiceGerman;
+    private HelloWorldService helloWorldServiceFrench;
 
     @Autowired
     public void setHelloWorldService(HelloWorldService helloWorldService) {
         this.helloWorldService = helloWorldService;
     }
 
+    @Autowired
+    @Qualifier("helloWorldServiceGerman")
+    public void setHelloWorldServiceGerman(HelloWorldService helloWorldServiceGerman) {
+        this.helloWorldServiceGerman = helloWorldServiceGerman;
+    }
+
+    @Autowired
+    @Qualifier("french")
+    public void setHelloWorldServiceFrench(HelloWorldService helloWorldServiceFrench) {
+        this.helloWorldServiceFrench = helloWorldServiceFrench;
+    }
+
     public String sayHello() {
         String greeting = helloWorldService.getGreeting();
         System.out.println(greeting);
+        System.out.println(helloWorldServiceGerman.getGreeting());
+        System.out.println(helloWorldServiceFrench.getGreeting());
         return greeting;
     }
 
